00c3ff2941c7c7e774dba39e8b525be67c57bab5
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2024-01-29 13:05:40 +0900

    DP

---
+++ DP/2×n 타일링.js
@@ -0,0 +1,18 @@
+// 2×n 타일링
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+// 입력
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const N = +input[0];
+const dp = Array(1001).fill(0);
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= N; i++) {
+  // 점화식 (dp[i - 1] + dp[i - 2]) % 10007 이용
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+// 결과 출력
+console.log(dp[N]);
