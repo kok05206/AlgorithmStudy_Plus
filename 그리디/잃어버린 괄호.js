007a4ecdfa6a8a425d4234ea9f2e17b960fae6a0
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2024-04-08 19:40:50 +0900

    잃어버린 괄호

--- 그리디/잃어버린 괄호.js
+++ 그리디/잃어버린 괄호.js
@@ -10,11 +10,9 @@ function solution(arr) {
   if (arr.length == 0) {
     return 0;
   } else {
-    // '+'로 sjplit 후 합산
-    return +arr.split('+').reduce((pre, cur) => +pre + +cur);
+    // '+'로 split 후 합산
+    return +arr.split('+').reduce((pre, cur) => +pre + +cur, 0);
   }
 }
 // 결과 출력
-console.log(
-  input.reduce((prev, cur) => prev - solution(cur), solution(input[0]) * 2)
-);
+console.log(input.reduce((prev, cur) => prev - solution(cur), solution(input[0]) * 2));
